d2e3afa7e4289f6189dd6f2a4ff37d0693ffa075
Taylor Christensen <chirstensentaylor6@gmail.com>
2025-07-15 21:24:29 -0400

    login page started

--- file_storage/storage_app/views.py
+++ file_storage/storage_app/views.py
@@ -1,8 +1,26 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def index(request):
-    return HttpResponse("This is a simple Django-powered file storage web app. Users will be able to upload files, view a list of uploaded files, and download them anytime. The project also includes pages like Home, About, and Contact to explain what the app does and how to get in touch.")
+    return render(request, 'storage_app/index.html')
 
 def login(request):
-    return HttpResponse("LOGIN PAGE")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')  # or wherever you want to go after login
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return redirect('login')
+    else:
+        return render(request, 'storage_app/login.html')
+
+def register(request):
+    return render(request, 'storage_app/register.html')
